4475ca1fc49d4cc4eec61ea8b60b778b2aa54c71
Yichen(Kelly) Chen <yichenchen.wings@gmail.com>
2023-06-02 18:49:14 +0100

    added shape and monotonicity test for test_MonotonicLayer

--- tests/test_monotonic_net.py
+++ tests/test_monotonic_net.py
@@ -11,9 +11,13 @@ def test_SingleVarPosMonotonic_shape():
 
 
 def test_SingleVarPosMonotonic_monotone():
-    batch_size = 50
-    func = SingleVarPosMonotonic(output_size=50)
-    xs = torch.rand(batch_size, 1, requires_grad=True)
+    batch_size = 20
+    func = SingleVarPosMonotonic(output_size=20)
+    xs = torch.subtract(
+        torch.rand(batch_size, 1),
+        0.5
+    )
+    xs.requires_grad = True
     out = func(x=xs)
     is_grad_non_neg = []
     for i in out:
@@ -23,4 +27,95 @@ def test_SingleVarPosMonotonic_monotone():
     assert all(is_grad_non_neg)
             
 
+def test_MonotonicLayer_single_shape():
+    batch_size = 2
+    z = torch.rand(batch_size, 3)
+    z0 = torch.rand(batch_size, 5)
+    ts = torch.rand(batch_size, 1)
+    gs = torch.rand(batch_size, 1)
+    func = MonotonicLayer(
+        input_size=z.shape[-1], 
+        z0_input_size=z0.shape[-1], 
+        output_size=3, 
+        act=torch.relu
+    )
+    out = func(z=z, z0=z0, t=ts, g=gs)
+    assert out.shape == (ts.shape[0], 3)
 
+
+def test_MonotonicLayer_multi_shape():
+    batch_size = 2
+    z = torch.zeros(batch_size, 3)
+    z0 = torch.rand(batch_size, 5)
+    ts = torch.rand(batch_size, 1)
+    gs = torch.rand(batch_size, 1)
+    
+    z1 = MonotonicLayer(
+        input_size=z.shape[-1], 
+        z0_input_size=z0.shape[-1], 
+        output_size=3, 
+        act=torch.relu
+    )(z=z, z0=z0, t=ts, g=gs)
+    
+    z2 = MonotonicLayer(
+        input_size=z1.shape[-1], 
+        z0_input_size=z0.shape[-1], 
+        output_size=2, 
+        act=torch.relu
+    )(z=z1, z0=z0, t=ts, g=gs)
+
+    out = MonotonicLayer(
+        input_size=z2.shape[-1], 
+        z0_input_size=z0.shape[-1], 
+        output_size=1, 
+        act=torch.relu
+    )(z=z2, z0=z0, t=ts, g=gs)
+
+    assert out.shape == (ts.shape[0], 1)
+
+
+def test_MonotonicLayer_multi_monotone():
+    batch_size = 2
+    z = torch.zeros(batch_size, 3)
+    z0 = torch.rand(batch_size, 5)
+    z0.requires_grad = True
+
+    ts = torch.rand(batch_size, 1)
+    ts.requires_grad = True
+
+    gs = torch.rand(batch_size, 1)
+    gs.requires_grad = True
+
+    z1 = MonotonicLayer(
+        input_size=z.shape[-1], 
+        z0_input_size=z0.shape[-1], 
+        output_size=30, 
+        act=torch.relu
+    )(z=z, z0=z0, t=ts, g=gs)
+    
+    z2 = MonotonicLayer(
+        input_size=z1.shape[-1], 
+        z0_input_size=z0.shape[-1], 
+        output_size=20, 
+        act=torch.relu
+    )(z=z1, z0=z0, t=ts, g=gs)
+
+    out = MonotonicLayer(
+        input_size=z2.shape[-1], 
+        z0_input_size=z0.shape[-1], 
+        output_size=1, 
+        act=torch.relu
+    )(z=z2, z0=z0, t=ts, g=gs)
+
+    if torch.cuda.is_available(): 
+        dev = "cuda:0" 
+    else: 
+        dev = "cpu" 
+
+    is_grad_non_neg = []
+    for i in out:
+        i.backward(retain_graph=True)
+        is_grad_non_neg.append(all(ts.grad >= 0))
+        is_grad_non_neg.append(all(gs.grad <= 0))
+    assert all(is_grad_non_neg)
+            
