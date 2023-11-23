1c996353492fbc9e5cf0e37d0875315d605a4f15
Yichen(Kelly) Chen <yichenchen.wings@gmail.com>
2023-11-23 22:50:04 +0000

    fixed a bug (missing space)

--- pop_sim_data/pop_train_hpc_sub.sh
+++ pop_sim_data/pop_train_hpc_sub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -J survsurf_one_surf_each_sigmoid_wide_hpc_tune
-#SBATCH --output=/home/yc366/rds/hpc-work/hpc_out/survsurf_one_surf_each_sigmoid_wide_hpc_tune/%a.out
-#SBATCH --error=/home/yc366/rds/hpc-work/hpc_out/survsurf_one_surf_each_sigmoid_wide_hpc_tune/%a.err
+#SBATCH -J survsurf_one_surf_each_sigmoid_wide_hpc_tune_wbias
+#SBATCH --output=/home/yc366/rds/hpc-work/hpc_out/survsurf_one_surf_each_sigmoid_wide_hpc_tune_wbias/%a.out
+#SBATCH --error=/home/yc366/rds/hpc-work/hpc_out/survsurf_one_surf_each_sigmoid_wide_hpc_tune_wbias/%a.err
 #SBATCH -A SCHONLIEB-SL3-GPU
 #SBATCH -p ampere
 #SBATCH --nodes 1
@@ -13,15 +13,15 @@
 module load rhel8/default-amp  
 source activate env_notebook_train_test_survsurf
 
-INPUT_DIR='/home/yc366/rds/hpc-work/data_markov_one_surf_each/'
+INPUT_DIR='/home/yc366/rds/hpc-work/data_markov_one_surf_each_wbias/'
 RUNTIME_DIR="/home/yc366/rds/hpc-work/hpc_out/${SLURM_JOB_NAME}/${SLURM_ARRAY_TASK_ID}_runtime/"
 OUTPUT_DIR="/home/yc366/rds/hpc-work/hpc_out/${SLURM_JOB_NAME}/${SLURM_ARRAY_TASK_ID}_results/"
 TUNE=y
 TUNE_LR=n
 N_TRIALS_TUNE=64
 N_EPOCHS_TUNE=100
-N_EPOCHS_TRAIN=200
-PATIENCE=50
+N_EPOCHS_TRAIN=1000
+PATIENCE=100
 WEIGHTED=y
 
 # Launch script using our defined variables
@@ -34,7 +34,7 @@ python ./pop_train_script.py  \
 --n_trials_tune $N_TRIALS_TUNE \
 --n_epochs_tune $N_EPOCHS_TUNE \
 --n_epochs_train $N_EPOCHS_TRAIN \
---patience $PATIENCE\
+--patience $PATIENCE \
 --weighted $WEIGHTED
 
 echo "Finished array: $SLURM_JOB_NAME $SLURM_ARRAY_TASK_ID"
