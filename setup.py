5a3fa1ae672e2e75c319e1170b533afa6dee46a4
Yichen(Kelly) Chen <yichenchen.wings@gmail.com>
2024-05-22 21:22:58 +0100

    updated setup.py for more stable package discovery

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 from monotonic_nn_surv_surf import __version__
 
 setup(
     name='monotonic_nn_surv_surf',
     version=__version__,
     author='Yichen Chen',
-    packages=find_packages(),
+    package_dir={
+        "monotonic_nn_surv_surf": "monotonic_nn_surv_surf"
+    }
+
 )
